ab17bc3714093d8986fbc42642b8186d6ad181cd
Tammy Cravit <tammy@tammymakesthings.com>
2022-04-06 17:24:13 -0700

    update project metadata

---
+++ scripts.py
@@ -0,0 +1,23 @@
+"""Poetry Script Shortcuts"""
+
+import subprocess
+
+
+def test():
+    """Run our unit tests"""
+    subprocess.run(['poetry', 'run', 'pytest', ], check=True)
+
+
+def black():
+    """Run black on the source and test files"""
+    subprocess.run(['poetry', 'run', 'black', 'tmt_carddeck', 'tests'], check=True)
+
+
+def isort():
+    """Run isort on the source and test files"""
+    subprocess.run(['poetry', 'run', 'isort', 'tmt_carddeck', 'tests', ], check=True)
+
+
+def check():
+    """Run our pre-commit checks"""
+    subprocess.run(['poetry', 'run', 'pre-commit', 'run', '--all-files', ], check=True)
