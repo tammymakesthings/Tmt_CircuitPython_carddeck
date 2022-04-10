833e4f0d89bfaae9e9a44087de9213ba491e5d42
Tammy Cravit <tammy@tammymakesthings.com>
2022-04-10 09:38:57 -0700

    Fixing pylint warnings

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -2,38 +2,81 @@
 #
 # SPDX-License-Identifier: MIT
 
-import pytest # noqa
-from tmt_carddeck.card import Card
-from tmt_carddeck.deck import Deck
+"""
+CircuitPython  Card Deck Library
+"""
+
+import pytest  # noqa
+
+from tmt_carddeck.card import Card  # pytest:disable=unused-import
+from tmt_carddeck.deck import (Deck, DeckEmpty)  # noqa pylint:disable=unused-import
+
 
 class TestDeck:
-    """
+    # Disable Pylint "method could be a function" errors
+    #
+    # pylint:disable=R0201
+
+    """Unit tests for the Deck class.
     """
 
-    def test_can_initialize_deck(self, starter_deck):
+    def test_can_initialize_deck(self, starter_deck) -> None:
+        """
+        Check that we can initialize a Deck object.
+
+        Args:
+            starter_deck (): the starter_deck pytest fixture
+        """
         assert isinstance(starter_deck, Deck)
         assert len(starter_deck) > 0
         assert [isinstance(the_card, Card) for the_card in starter_deck.cards]
 
-    def test_can_pick_cards(self, starter_deck):
+    # Test picking cards - with and without deck reset.
+    def test_pick_cards(self, starter_deck) -> None:
+        """
+        Check that we can pick cards from the Deck.
+
+        Args:
+            starter_deck (): the starter_deck pytest fixture
+        """
         for _ in range(len(starter_deck) - 1):
             assert isinstance(starter_deck.pick(), Card)
 
-    def test_deck_resets_on_empty(self, starter_deck):
+    def test_reset_on_empty(self, starter_deck) -> None:
+        """
+        Check that the Deck resets if it's empty.
+
+        Args:
+            starter_deck (): the starter_deck pytest fixture
+        """
         for _ in range(len(starter_deck)):
             starter_deck.pick()
         assert len(starter_deck) == 0
         picked_card = starter_deck.pick()
         assert isinstance(picked_card, Card)
 
-    def test_deck_raises_exception_on_non_reset(self, starter_deck):
+    def test_raise_exception_no_reset(self, starter_deck) -> None:
+        """
+        Check that when the deck is empty and reset_if_empty is false,
+        a DeckEmptyError is raised.
+
+        Args:
+            starter_deck (): the starter_deck pytest fixture
+        """
         for _ in range(len(starter_deck)):
             starter_deck.pick()
         assert len(starter_deck) == 0
-        with pytest.raises(ValueError):
+        with pytest.raises(DeckEmpty):
             _ = starter_deck.pick(reset_if_empty=False)
 
-    def test_dunder_len(self, starter_deck):
+    # Test sequence methods®
+    def test_dunder_len(self, starter_deck) -> None:
+        """
+        Check that `len(deck)` works.
+
+        Args:
+            starter_deck (): the starter_deck pytest fixture
+        """
         assert len(starter_deck) > 0
         for _ in range(len(starter_deck)):
             starter_deck.pick()
--- tmt_carddeck/deck.py
+++ tmt_carddeck/deck.py
@@ -6,38 +6,83 @@
 tmt_carddeck: CircuitPython Card Deck library.
 """
 
-from collections import namedtuple   # noqa
-from tmt_carddeck.card import Card   # noqa
+from tmt_carddeck.card import Card  # noqa
+
 
 try:
-    from typing import List, Optional, Union, Tuple  # noqa
+    from typing import List, Optional  # noqa
 except ImportError:
     pass
 
+
+class DeckEmpty(RuntimeWarning):
+    """Raised when the deck is empty and reset_if_empty is false.
+    """
+
+
 class Deck:
     """
+    Represents a deck of cards.
     """
+
     def __init__(self, initial_cards: Optional[List[Card]] = None) -> None:
+        """
+        Initialize a new Deck
+
+        Args:
+            initial_cards (Optional[list[Card]]): The initial list of cards.
+        """
         self._initial_cards = list(initial_cards)
         self._cards = []
         self.reset_deck()
 
     def reset_deck(self) -> None:
+        """
+        Reset the deck to the initial cards.
+        Returns:
+
+        """
         self._cards = list(self._initial_cards)
 
     @property
     def cards(self) -> Optional[List[Card]]:
+        """
+        Retrieve a reference to the deck's cards. We return a reference so
+        the caller can modify the deck if desired.
+
+        Returns:
+        A reference to the deck's current contents.
+        """
         return self._cards
 
-    def pick(self,
-             pick_location: Optional[int] = 0,
-             reset_if_empty: Optional[bool] = True) -> Card:
+    def pick(self, **kwargs):
+        """
+        Pick a card from the deck.
+        Args:
+            kwargs: The argument dictionary:
+                pick_location (int): The pick location within the stack of cards.
+                reset_if_empty (bool): True to reset the deck to its initial
+                    state if it's empty.
+
+        Returns:
+            The selected card.
+
+        Raises:
+            `DeckEmpty` is raised if the deck is empty and reset_if_empty
+            is False.
+        """
+        reset_if_empty: bool = kwargs.get('reset_if_empty', True)
+        pick_location: int = kwargs.get('pick_location', 0)
+
         if len(self._cards) == 0:
             if reset_if_empty:
                 self.reset_deck()
             else:
-                raise ValueError('no cards in deck')
+                raise DeckEmpty('no cards in deck')
         return self._cards.pop(pick_location)
 
     def __len__(self):
+        """
+        Get the number of cards in the deck.
+        """
         return len(self._cards)
